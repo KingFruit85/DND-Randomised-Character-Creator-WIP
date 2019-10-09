cc15759b03e9104832cd01ccf9f0a0c4202f360b
Christopher Long <christopher.aaron.long@gmail.com>
2019-10-09 18:34:28 +0100

    Updated armor objects

--- armor.js
+++ armor.js
@@ -1,111 +1,141 @@
 const lightArmor = {
-    no_armor: {
-        name: "No Armor"
-    },
+
     padded: {
         name: "Padded Armor",
         Cost: "5gp",
-        ArmorClass: "11 + DEX",
-        StrengthRequirement: "",
-        Stealth: "Disadvantage",
+        baseArmorClass:11,
+        additionalModifier:"DEX",
+        modifierLimit:null,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:true,
         Weight: "8lb"
     },
     leather: {
         name: "Leather Armor",
         Cost: "10 gp",
-        ArmorClass: "11 + DEX",
+        baseArmorClass: 11,
+        additionalModifier:"DEX",
+        modifierLimit:null,
+        strengthRequirement:false,
+        stealthDisadvantage:false,
         Weight: "10lb"
     },
     studdedLeather: {
         name: "Studded Leather Armor",
         Cost: "45 gp",
-        ArmorClass: "12 + DEX",
-        StrengthRequirement: "",
-        Stealth: "",
+        baseArmorClass:12,
+        additionalModifier:"DEX",
+        modifierLimit:null,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:false,
         Weight: "13lb"
     }
-}
+};
 
 const mediumArmor = {
 
     hide: {
         name: "Hide Armor",
         Cost: "10gp",
-        ArmorClass: "12 + DEX(Max 2)",
-        StrengthRequirement: "",
-        Stealth: "",
+        baseArmorClass:12,
+        additionalModifier:"DEX",
+        modifierLimit:2,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:false,
         Weight: "12lb"
     },
     chainShirt: {
         name: "Chain Shirt",
         Cost: "50gp",
-        ArmorClass: "13 + DEX(max 2)",
-        StrengthRequirement: "",
-        Stealth: "",
+        baseArmorClass:13,
+        additionalModifier:"DEX",
+        modifierLimit:2,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:false,
         Weight: "20lb"
     },
     scaleMail: {
         name: "Scale Mail",
         Cost: "50gp",
-        ArmorClass: "14 + DEX(max 2)",
-        StrengthRequirement: "",
-        Stealth: "Disadvantage",
+        baseArmorClass:14,
+        additionalModifier:"DEX",
+        modifierLimit:2,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:true,
         Weight: "45lb"
     },
     breastplate: {
         name: "Breastplate",
         Cost: "400gp",
-        ArmorClass: "15 + DEX(max 2)",
-        StrengthRequirement: "",
-        Stealth: "Disadvantage",
+        baseArmorClass:15,
+        additionalModifier:"DEX",
+        modifierLimit:2,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:false,
         Weight: "40lb"
     },
     halfPlate: {
         name: "HalfPlate",
         Cost: "750gp",
-        ArmorClass: "11 + DEX",
-        StrengthRequirement: "",
-        Stealth: "",
+        baseArmorClass:11,
+        additionalModifier:"DEX",
+        modifierLimit:null,
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage:true,
         Weight: "8lb"
     }
-}
+};
 
 const heavyArmor = {
 
     ringMail: {
         name: "Ring Mail",
         Cost: "30 gp",
-        ArmorClass: "14",
-        StrengthRequirement: "—",
-        Stealth: "Disadvantage",
+        baseArmorClass: "14",
+        strengthRequirement:false,
+        strengthRequirementValue:null,
+        stealthDisadvantage: true,
         Weight: "40 lb"
     },
     chainMail: {
         name: "Chain Mail",
         Cost: "75 gp",
-        ArmorClass: "16",
-        StrengthRequirement: "STR 13",
-        Stealth: "Disadvantage",
+        baseArmorClass: "16",
+        additionalModifier:null,
+        strengthRequirement:true,
+        strengthRequirementValue:13,
+        stealthDisadvantage: true,
         Weight: "55 lb"
     },
     splint: {
         name: "Splint Mail",
         Cost: "200 gp",
-        ArmorClass: "17",
-        StrengthRequirement: "STR 15",
-        Stealth: "Disadvantage",
+        baseArmorClass: "17",
+        additionalModifier:null,
+        strengthRequirement:true,
+        strengthRequirementValue:15,
+        stealthDisadvantage: true,
         Weight: "60 lb"
     },
     plate: {
         name: "Plate Mail",
         Cost: "1500 gp",
-        ArmorClass: "18",
-        StrengthRequirement: "STR 15",
-        Stealth: "Disadvantage",
+        baseArmorClass: "18",
+        additionalModifier:null,
+        strengthRequirement:true,
+        strengthRequirementValue:15,
+        stealthDisadvantage: true,
         Weight: "65 lb"
     },
 
-}
+};
 
 const shields = {
     shield: {
