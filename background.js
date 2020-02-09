98b1043d2b018bbb43ea1533e322bf768d176190
HedgehogHolly <hollyejallen@gmail.com>
2020-02-09 12:44:13 +0000

    bond

---
+++ background.js
@@ -0,0 +1,22 @@
+const utils = require("./utils.js");
+
+const bond = {
+    one: {
+        description: "I would die to recover an ancient relic of my faith that was lost long ago."
+    },
+    two: {
+        description: "I will someday get revenge on the corrupt temple hierarchy who branded me a heretic."
+    },
+    three: {
+        description: "I owe my life to the priest who took me in when my parents died."
+    },
+    four: {
+        description: "Everything I do is for the common people."
+    },
+    five: {
+        description: "I will do anything to protect the temple where I served."
+    },
+    six: {
+        description: "	I seek to preserve a sacred text that my enemies consider heretical and seek to destroy"
+    },
+}, 
